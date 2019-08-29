7d362944019973484074109e55e064f7ec9051be
chenxiangsi <chenxiangsi@jasgroup.cn>
2019-08-29 14:20:45 +0800

    fix:权限管理页面修改

--- jasmvvm/pages/module-privilege-easyui/role/js/addRole.js
+++ jasmvvm/pages/module-privilege-easyui/role/js/addRole.js
@@ -58,7 +58,7 @@ function loadData() {
 					loadSelect(jsonData);
 				},
 				error : function(result) {
-					top.showAlert(getLanguageValue("tip"), getLanguageValue("queryError"), 'info');
+					window.showAlert(getLanguageValue("tip"), getLanguageValue("queryError"), 'info');
 				}
 			});
 		} 
@@ -83,7 +83,7 @@ function loadSelect(jsonData){
  * @desc 关闭添加页面
  */
 function closePanel() {
-	top. closeDlg("saveiframe");
+	window.closeDlg("saveiframe");
 }
 
 /**
@@ -103,7 +103,7 @@ function save() {
 	var fromData = JSON.stringify(formData);//获取表单中的json,
 	
 	if(validateResault == false){
-		top.showAlert(getLanguageValue("tip"), getLanguageValue("formVailidateFailed"), 'info');
+		window.showAlert(getLanguageValue("tip"), getLanguageValue("formVailidateFailed"), 'info');
 		enableButtion("saveButton");
 		return validateResault;
 	}else{
@@ -115,14 +115,14 @@ function save() {
 		    data:fromData,
 		    success: function(data){
 				if(data.status==1){
-					top.showAlert(getLanguageValue("tip"), getLanguageValue("savesuccess"), 'info', function() {
+					window.showAlert(getLanguageValue("tip"), getLanguageValue("savesuccess"), 'info', function() {
 						//重新加载表格数据
 						reloadData('queryRole.htm', '#dg');
 						//关闭弹出框
 					    closePanel();
 					});
 				} else {
-					top.showAlert(getLanguageValue("tip"), data.msg, 'error');
+					window.showAlert(getLanguageValue("tip"), data.msg, 'error');
 					enableButtion("saveButton");
 				}
 		    }
