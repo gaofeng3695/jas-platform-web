f641538a42f9bf1f6576b0392b12674ef2278b1e
gaofeng <gaofeng3695@126.com>
2020-01-15 11:48:44 +0800

    添加节流与防抖函数

--- jasmvvm/common/js/jas-tools.js
+++ jasmvvm/common/js/jas-tools.js
@@ -422,7 +422,7 @@
 			return inst;
 		};
 
-		var encryption = function(txt){
+		var encryption = function (txt) {
 			var _str = txt;
 			var _strLength = _str.length;
 			var _strTarget = "";
@@ -431,6 +431,67 @@
 			}
 			return escape(_strTarget);
 		};
+
+		/**
+		 * @desc 函数防抖
+		 * @param func 函数
+		 * @param wait 延迟执行毫秒数
+		 * @param immediate true 表立即执行，false 表非立即执行
+		 */
+		var debounce = function (func, wait, immediate) {
+			let timeout;
+
+			return function () {
+				let context = this;
+				let args = arguments;
+
+				if (timeout) clearTimeout(timeout);
+				if (immediate) {
+					var callNow = !timeout;
+					timeout = setTimeout(() => {
+						timeout = null;
+					}, wait)
+					if (callNow) func.apply(context, args)
+				} else {
+					timeout = setTimeout(function () {
+						func.apply(context, args)
+					}, wait);
+				}
+			}
+		};
+
+		/**
+		 * @desc 函数节流
+		 * @param func 函数
+		 * @param wait 延迟执行毫秒数
+		 * @param type 1 表时间戳版，2 表定时器版
+		 */
+		var throttle = function (func, wait, type) {
+			if (type === 1) {
+				let previous = 0;
+			} else if (type === 2) {
+				let timeout;
+			}
+			return function () {
+				let context = this;
+				let args = arguments;
+				if (type === 1) {
+					let now = Date.now();
+
+					if (now - previous > wait) {
+						func.apply(context, args);
+						previous = now;
+					}
+				} else if (type === 2) {
+					if (!timeout) {
+						timeout = setTimeout(() => {
+							timeout = null;
+							func.apply(context, args)
+						}, wait)
+					}
+				}
+			}
+		}
 
 		return {
 			rootPath: getRootPath(),
@@ -448,6 +509,9 @@
 			fileUploader: fileUploader,
 			fileLister: fileLister,
 			encryption: encryption,
+			encryption: encryption,
+			debounce: debounce,
+			throttle: throttle,
 		};
 	})();
 
